233761d865702e61918fc6adb1e7169c868c82a3
Gabriel Moura <gabriel.lima@aspec.com.br>
2025-07-23 11:45:43 -0300

    feat: :art: add image domains configuration to next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  images: {
+    domains: [
+      process.env.NEXT_PUBLIC_SUPABASE_DOMAIN ||
+        'ctxbakysvmnwewronovf.supabase.co',
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
